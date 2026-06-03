af1cd9237c11bd4a473da5bbe87ae949e5b2a1f8
BuildWithXin <duxttt555@gmail.com>
2026-06-03 16:19:14 -0700

    Add config.js for deployment

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Xin Running 配置文件
+ * 请在高德开放平台申请 Web 端 (JS API) Key：https://lbs.amap.com/
+ */
+window.XinRunningConfig = {
+  /** 高德地图 Web 端 Key */
+  amapKey: "83d4983c54dcfa9c6cde6309423350d8",
+
+  /**
+   * 安全密钥（2021 年 12 月后申请的 Key 需填写）
+   * 留空则不启用 _AMapSecurityConfig
+   */
+  amapSecurityCode: ""
+};
